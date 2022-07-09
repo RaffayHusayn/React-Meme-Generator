004e3a6a1843e1ea8c5f4796c376b52ae52cfc69
Raffay Syed <raffayhusayn@gmail.com>
2022-07-09 00:15:56 -0400

    Add code to correctly use useState's setter when the new state value is based on the previous value

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,14 @@ import Pepe from "../images/pepe.png";
 function Navbar() {
   let [logo, setLogo] = useState(1);
   function onImgClick() {
+    // if the new state doesn't depend on the prev state than we don't
+    // need a callback function inside the setter
     setLogo("Don't touch me, Creep");
     setTimeout(() => {
-      logo++;
-      setLogo(logo);
+      //changing the state based on prev State is done using
+      // a callback inside the setter instead of directly changing
+      // it
+      setLogo((prevLogo) => prevLogo + 1);
     }, 1000);
   }
   return (

150d32705fd0a6b6c22f89fdd583a4ca9561e6ce
Raffay Syed <raffayhusayn@gmail.com>
2022-07-11 01:59:20 -0400

    Add a react hook to load a new image when the "get new meme" button is clicked

--- src/data/MemeData.jsx
+++ src/data/MemeData.jsx
@@ -3,13 +3,15 @@ const MemeData = {
   data: {
     memes: [
       {
-        id: 01,
+        id: 1,
         url: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2776&q=80",
       },
       {
-        id: 02,
+        id: 2,
         url: "https://images.unsplash.com/photo-1549545931-59bf067af9ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80",
       },
     ],
   },
 };
+
+export default MemeData;
